29d878659abe250b7f1f33e8d895cdaa396f20b8
bernandoLR <bernandolumbanraja02@gmail.com>
2020-08-24 09:33:08 +0700

    update build script

--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# This command for build image and tag image to bernando/backend.
+# You can change tag to you docker registry
 cd back-end && docker build -t bernando/backend .
+
+# This command for build image and tag image to bernando/backend.
+# You can change tag to you docker registry
 cd ../front-end && docker build -t bernando/frontend .
 
+# This command for push image to you docker registry
 docker push bernando/backend
+
+# This command for push image to docker registry
 docker push bernando/frontend
 
 
+# This command for install helm chart with name pintu
 cd .. && helm install pintu ./helm-pintu
